85235b1a1ec74f00472ea0c45229a78dfc32414e
Samnotwise <116502480+sam8314@users.noreply.github.com>
2024-02-22 11:04:04 +0100

    Update menus.c

--- final/menus.c
+++ final/menus.c
@@ -4,7 +4,7 @@
 /*0 : welcome animation
 1 : home menu
 2 : leaderboard
-3 : rules page 1
+3 : rules
 4 : select player mode
 5 : select difficulty
 6 : game setup
@@ -12,94 +12,127 @@
 fix: change home menu : BTN1 is untouchable
 */
 
-void navigation(int page){
-    int buttons = get_btns();
 
-    if(page == 0){currentPage=welcome_animation(buttons);}
-    else if(page==1){currentPage=home_menu(buttons);}
-    else if(page==2){currentPage=leaderboard(buttons);}
-    else if(page==3){currentPage=rules1(buttons);}
-    else if(page==4){currentPage=select_player_mode(buttons);}
-    else if(page==5){currentPage=select_difficulty(buttons);}
-}
-
-int welcome_animation(buttons){
+void welcome_animation(){
     display_string(2,"welcome animation");
-    return 1; // go to home menu
 }
 
-int leaderboard(buttons){
-    display_string(2, "leaderboard");
-    return 1;
-}
-
-int home_menu(buttons){
-    display_string(0, "HOME MENU");
-    display_string(1, "leaderboard press BTN2");
-    display_string(2, "how to play press BTN3");
-    display_string(3, "select game mode press BTN4");
+void home_menu(int buttons, int exit){
+    if(currentPage==1)
+    {
+        display_string(0, "HOME MENU");
+        display_string(1, "leaderboard BTN2");
+        display_string(2, "how to play BTN3");
+        display_string(3, "game mode   BTN4");
+    }
 
     if(buttons & 1) //BTN2
     {
-        return 2;// go to leaderboard
+        currentPage=2;
     }
     else if(buttons & 2) //BTN3
     {
-        return 3;//go to page 1 of rules
+        currentPage= 3;//go to page 1 of rules
     }
     else if(buttons & 4) //BTN4
     {
-        return 4;//go select player mode
+        currentPage= 4;//go select player mode
     }
+
+
 }
 
-int select_player_mode(buttons){
-    display_string(0, "go back home BTN1");
-    display_string(2, "single player BTN2");
+void leaderboard(int buttons, int exit){
+    if(exit)
+    {
+        currentPage=1;
+        return;
+    }
+
+    display_string(0,"");
+    display_string(1,"");
+    display_string(2, "leaderboard");
+    display_string(3,"");
+
+
+}
+
+void select_player_mode(int buttons, int exit){
+    if(exit)
+    {
+        currentPage=1;
+        return;
+    }
+
+    display_string(0, "return home BTN1");
+    display_string(1,"");
+    display_string(2, "one player  BTN2");
     display_string(3, "two players BTN3");
 
-    /*is there a cleaner way of coding this?*/
     if(buttons & 1) //BTN2
     {
         numberOfPlayers=1;
-        return 5; // go select difficulty
+        currentPage = 5; // go select difficulty
     }
     else if(buttons & 2) //BTN3
     {
         numberOfPlayers = 2;
-        return 5; //go select difficulty
+        currentPage= 5; //go select difficulty
     }
+
+
 }
 
-int select_difficulty(buttons){
-    display_string(0, "go back home BTN1");
-    display_string(1, "easy BTN2");
-    display_string(2, "medium BTN3");
-    display_string(3, "hard BTN4");
+void select_difficulty(int buttons, int exit){
+    if(exit)
+    {
+        currentPage=4;
+        return;
+    }
+    display_string(0, "change mode BTN1");
+    display_string(1, "easy        BTN2");
+    display_string(2, "medium      BTN3");
+    display_string(3, "hard        BTN4");
 
     if(buttons & 1) //BTN2
     {
         difficultyLevel = 1;
-        return 3;// go to rules page 1
+        currentPage= 6;// go to game setup
     }
     else if(buttons & 2) //BTN3
     {
         difficultyLevel = 2;
-        return 4;//go select difficulty
+        currentPage= 6;//go game setup
     }
     else if(buttons & 4) //BTN4
     {
-        return 1;//go to home menu
+        difficultyLevel = 3;
+        currentPage= 6;//go to game setup
+    }
+}
+
+void rules(int buttons, int exit){
+    if(exit)
+    {
+        currentPage=1;
+        return;
     }
 
+    display_string(0, "RULES");
+    display_string(1, "");
+    display_string(2, "");
+    display_string(3, "");
 }
 
-int rules1(buttons){
-    //...
-}
+void navigation(int page){
+    int buttons = get_btns();
+    int exit = get_exit();
 
-int rules2(buttons){
-    //...
+    if(page==1){home_menu(buttons, exit);}
+    else if(page==2){leaderboard(buttons, exit);}
+    else if(page==3){rules(buttons, exit);}
+    else if(page==4){select_player_mode(buttons, exit);}
+    else if(page==5){select_difficulty(buttons, exit);}
 }
 
 
