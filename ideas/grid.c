5a20a42dede601159f64b7b51917203cf5adca6f
Kinga Koltai <160338784+kinga-anna@users.noreply.github.com>
2024-02-26 10:54:34 +0100

    Create grid.c

---
+++ ideas/grid.c
@@ -0,0 +1,139 @@
+#include <stdint.h> 
+#include <pic32mx.h>
+#include "mipslab.h"
+
+#define DISPLAY_CHANGE_TO_COMMAND_MODE (PORTFCLR = 0x10)
+#define DISPLAY_CHANGE_TO_DATA_MODE (PORTFSET = 0x10)
+
+#define DISPLAY_ACTIVATE_RESET (PORTGCLR = 0x200)
+#define DISPLAY_DO_NOT_RESET (PORTGSET = 0x200)
+
+#define DISPLAY_ACTIVATE_VDD (PORTFCLR = 0x40)
+#define DISPLAY_ACTIVATE_VBAT (PORTFCLR = 0x20)
+
+#define DISPLAY_TURN_OFF_VDD (PORTFSET = 0x40)
+#define DISPLAY_TURN_OFF_VBAT (PORTFSET = 0x20)
+
+uint8_t data[] = {
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    255,255,255,255,255,255,255,255,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,
+};
+
+
+void display_image2(int x, uint8_t *data) {
+	int i, j;
+
+	for(i = 0; i < 4; i++) {
+		DISPLAY_CHANGE_TO_COMMAND_MODE;
+
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+
+		spi_send_recv(x & 0xF);
+		spi_send_recv(0x10 | ((x >> 4) & 0xF));
+
+		DISPLAY_CHANGE_TO_DATA_MODE;
+
+		for(j = 0; j < 128; j++)
+			spi_send_recv(~data[i*128 + j]);
+	}
+}
+//needs a helper function that returns 4 8-bit numbers from 1 array with 30 ints
+void generate_grid(int offset){
+    for (int col = offset; col<128-offset; col++)
+    {
+        int number[32]={1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        for (int row = 1; row<31; row++)
+        {
+            int r = rand() % 4;
+            for (r;r>0;r--){ //count down from random number to 1
+                if (row<31){ //if we're still on the screen (between rows 1 and 30), write it to array
+                    number[row] = 0;
+                    row++;
+                }
+            }
+            row++; //have an empty line between objects
+        }
+        //write it in data as a uint8_t through a helper function that does the conversion
+    }
+}
+
+uint8_t helper(int number[32]){
+    
+}
+
+/* idea:
+    - initialize array to all 255
+    - we start with an offset dependent of the game mode
+    - we generate columns 128-2*offset times in binary (0 for dot, 1 for no dot?)
+    - we convert these to uint_8 values
+    - and we write those to the correct index of the array */
